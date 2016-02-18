8f41417c10dbf109b3de146ae9bd53894f9e93ab
manidf <mannuel.ferreira@gmail.com>
2016-02-18 22:40:45 +0200

    IFFE

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-(function () {
 
+(function() {
   renderPage();
+
   function renderPage() {
     var template = $('#index-template').html(),
         compiled = Handlebars.compile(template),
         rendered = compiled(window.language);
     $('#main').html(rendered);
   }
+
   $('#languageSwitch').click(function () {
     DogPack.switchLanguage();
   });
-
 })();
